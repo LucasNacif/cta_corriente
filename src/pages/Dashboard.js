197680c78a5c143db1278dafd28d1481fe1a0207
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-12-03 00:50:44 -0300

    feature/ card y tabla del dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -38,17 +38,41 @@ export default function Dashboard() {
         fetchMovimientos();
         fetchCuentas();
     }, []);
-
-    // Ajustar columnas a mostrar en tabla
-    const columns = [
-        { header: 'Importe Movimiento', align: 'text-center' },
-        { header: 'Importe Impago', align: 'text-center' },
-        { header: 'Medio de Pago', align: 'text-center' },
-    ];
-    
-    const [balance] = useState(15000);
     const userName = Cookies.get('name');
     const totalCuentas = cuentas.length;
+
+    // Calcular el total de los movimientos (importeMovimiento y importePagado)
+    const totalImporteMovimiento = movimientos.reduce(
+        (total, movimiento) => total + movimiento.importeMovimiento,
+        0
+    );
+
+    const totalImportePagado = movimientos.reduce(
+        (total, movimiento) => total + movimiento.importePagado,
+        0
+    );
+
+    // Agrupar las cuentas por la cantidad de movimientos
+    const cuentasConMasMovimientos = cuentas.map((cuenta) => {
+        const movimientosCuenta = movimientos.filter(
+            (mov) => mov.cuentaId === cuenta.id
+        );
+        return {
+            cuentaNombre: cuenta.name,
+            cantidadMovimientos: movimientosCuenta.length,
+        };
+    });
+
+    // Ordenar las cuentas por la cantidad de movimientos de mayor a menor
+    const cuentasOrdenadas = cuentasConMasMovimientos
+        .sort((a, b) => b.cantidadMovimientos - a.cantidadMovimientos)
+        .slice(0, 5); 
+
+    const columns = [
+        { header: 'Cuenta', align: 'text-center' },
+        { header: 'Cantidad de Movimientos', align: 'text-center' },
+    ];
+
 
     return (
         <div className="container mx-auto">
@@ -59,21 +83,25 @@ export default function Dashboard() {
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <Card className="p-4 border-none bg-gradient-to-t from-monza-500 to-monza-900 text-monza-100">
                     <div className="flex flex-row items-center justify-between pb-2">
-                        <span className="text-sm font-medium">Patrimonio</span>
+                        <span className="text-sm font-medium">Total Movimiento</span>
                         <DollarSign className="h-4 w-4 text-muted-foreground" />
                     </div>
                     <div>
-                        <div className="text-4xl font-bold py-3">${balance.toLocaleString()}</div>
+                        <div className="text-4xl font-bold py-3">
+                            ${totalImporteMovimiento.toLocaleString()}
+                        </div>
+                        <Link to="/movimientos" className="text-xs font-semibold text-gray-100 flex items-center"> <CornerDownRight className='mr-2 w-5' />Ver movimientos</Link>
                     </div>
                 </Card>
-                <Card className='p-4 border-none bg-gradient-to-t from-blue-500 to-blue-900 text-blue-100'>
+                <Card className="p-4 border-none bg-gradient-to-t from-blue-500 to-blue-900 text-blue-100">
                     <div className="flex flex-row items-center justify-between pb-2">
-                        <span className="text-sm font-medium">Movimientos Pendientes</span>
-                        <ArrowLeftRight className="h-4 w-4 text-muted-foreground" />
+                        <span className="text-sm font-medium">Total Pagado</span>
+                        <DollarSign className="h-4 w-4 text-muted-foreground" />
                     </div>
                     <div>
-                        <div className="text-3xl font-bold py-3">3</div>
-                        <Link to="/movimientos" className="text-xs font-semibold text-gray-200 flex items-center"> <CornerDownRight className='mr-2 w-5' />Ver movimientos</Link>
+                        <div className="text-4xl font-bold py-3">
+                            ${totalImportePagado.toLocaleString()}
+                        </div>
                     </div>
                 </Card>
                 <Card className='p-4 border-zinc-400 bg-zinc-100'>
@@ -90,35 +118,29 @@ export default function Dashboard() {
 
             <div className="mt-4 mb-10 grid gap-4 md:grid-cols-2 lg:grid-cols-7">
                 <div className="col-span-4">
-                    <h2 className="text-lg font-semibold my-2">Resumen de Cuenta</h2>
+                    <h2 className="text-lg font-semibold my-2">Cuentas con Más Movimientos</h2>
                     {error ? (
                         <div className="fixed top-5 left-1/2 transform -translate-x-1/2 z-50">
                             <div className="bg-monza-700 text-monza-200 rounded-lg shadow-lg px-4 py-2 flex items-center space-x-4">
                                 <p className="text-sm">{error}</p>
-                                <button
-                                    className="ml-auto flex items-center justify-center text-white hover:bg-red-800 rounded-full px-1"
-                                    onClick={() => setError(null)}
-                                >
-                                    <X className='w-4' />
-                                </button>
                             </div>
                         </div>
                     ) : (
                         <div className="overflow-y-auto max-h-[600px]">
                             <Table
                                 columns={columns}
-                                data={movimientos.slice(0, 10)}
-                                renderRow={(movimiento) => (
+                                data={cuentasOrdenadas}
+                                renderRow={(cuenta) => (
                                     <>
-                                        <td className="px-4 py-3 text-center">${movimiento.importeMovimiento}</td>
-                                        <td className="px-4 py-3 text-center">${movimiento.importeImpago}</td>
-                                        <td className="px-4 py-3 text-center">{movimiento.medioPago}</td>
+                                        <td className="px-4 py-3 text-center">{cuenta.cuentaNombre}</td>
+                                        <td className="px-4 py-3 text-center">{cuenta.cantidadMovimientos}</td>
                                     </>
                                 )}
                             />
                         </div>
                     )}
                 </div>
+
                 <div className="col-span-3">
                     <h2 className="text-lg font-semibold py-2">Alertas</h2>
                     <Card className="p-4 min-h-full border-zinc-400/40 bg-zinc-100">
